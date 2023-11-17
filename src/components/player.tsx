d2e755973509474153134b6707bc7893ffc32eb0
Hamalain3n <thomas.hamalainen@live.se>
2023-11-17 15:55:03 +0100

    changed from app to pages

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,19 +1,15 @@
 
-function Player (videoCode: any)  {
-  function url():string {
-    const url = `https://www.youtube.com/embed/${videoCode.videoCode}`
-    return url
+function Player(videoCode: any) {
+  function url(): string {
+    const url = `https://www.youtube.com/embed/${videoCode.videoCode}`;
+
+    return url;
   }
   return (
     <div>
-      <iframe
-        width="420"
-        height="345"
-        src={url()}
-      ></iframe>
+      <iframe width="420" height="345" src={url()}></iframe>
     </div>
   );
-};
+}
 
 export default Player;
- 
