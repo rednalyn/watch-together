791d4aec3b204f91c93916b7c559d4cedc67f018
Hamalain3n <thomas.hamalainen@live.se>
2023-11-17 16:44:12 +0100

    woring on socket.io

--- src/app/[room]/page.tsx
+++ src/app/[room]/page.tsx
@@ -1,52 +1,19 @@
-"use client"
-import { useEffect, useState } from 'react'
-import io from 'socket.io-client'
-let socket:any;
-
-const Home = () => {
-  const [input, setInput] = useState('')
-
-  useEffect(() => {socketInitializer()}, [])
-
-  const socketInitializer = async () => {
-    await fetch('/api/socket');
-    socket = io()
-
-    socket.on('connect', () => {
-      console.log('connected')
-    })
-
-    socket.on('update-input', msg => {
-      setInput(msg)
-    })
-  }
-
-  const onChangeHandler = (e:any) => {
-    setInput(e.target.value)
-    socket.emit('input-change', e.target.value)
-  }
-
+"use client";
+import Player from "@/src/components/player";
+export default function room({ params }: { params: { room: string } }) {
+  let videoCode = "DLXwnPMbivE";
   return (
-    <input
-      placeholder="Type something"
-      value={input}
-      className='text-black'
-      onChange={onChangeHandler}
-    />
-  )
+    <main>
+      <h1>room: {params.room}</h1>
+      <button
+        onClick={() => {
+          videoCode = "EpxrDorr1i8";
+        }}
+      >
+        annnanna videeeo
+      </button>
+      <Player videoCode={videoCode} />
+      <div id="player"></div>
+    </main>
+  );
 }
-
-export default Home;
-// "use client"
-// import Player from "@/src/components/player";
-// export default function room({ params }: { params: { room: string } }) {
-//   let videoCode = "DLXwnPMbivE"
-//   return (
-//     <main>
-//       <h1>room: {params.room}</h1>
-//       <button onClick={() => {videoCode = "EpxrDorr1i8"}} >annnanna videeeo</button>
-//       <Player videoCode={videoCode} />
-//       <div id="player"></div>
-//     </main>
-//   );
-// }
--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,15 +1,76 @@
 
-function Player(videoCode: any) {
-  function url(): string {
-    const url = `https://www.youtube.com/embed/${videoCode.videoCode}`;
+"use client"
+// ts
+import React from 'react';
+import YouTube, { YouTubePlayer, YouTubeProps } from 'react-youtube';
+import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+let socket:any;
 
-    return url;
+export default function Player(videoCode:any) {
+
+    const [input, setInput] = useState('')
+
+    useEffect(() => {socketInitializer()}, [])
+  
+    const socketInitializer = async () => {
+      await fetch('/api/socket');
+      socket = io()
+  
+      socket.on('connect', () => {
+        console.log('connected')
+      })
+  
+    //   socket.on('update-input', msg => {
+    //     setInput(msg)
+    //   })
+    // }
+  
+    // const onChangeHandler = (e:any) => {
+    //   setInput(e.target.value)
+    //   socket.emit('input-change', e.target.value)
+    // }
+
+    socket.on('play', (msg:any) => {
+        console.log("msg")
+        const element = document.getElementById('movie_player');
+        element?.dispatchEvent(new KeyboardEvent('keydown', {'key': 'k'}));
+      })
+
+      socket.on('pause', (msg:any) => {
+        console.log("msg")
+        const element = document.getElementById('movie_player');
+        element?.dispatchEvent(new KeyboardEvent('keydown', {'key': 'k'}));
+      })
+
+    }
+
+
+  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
   }
-  return (
-    <div>
-      <iframe width="420" height="345" src={url()}></iframe>
-    </div>
-  );
-}
+  const onPlayerPause: YouTubeProps['onPause'] = (event) => {
+    socket.emit('pause', "pause")
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+  const onPlayerPlay: YouTubeProps['onPlay'] = (event) => {
+    console.log('onplay')
+    socket.emit('play', "play")
+    // access to player in all event handlers via event.target
+    event.target.playVideo();
+  }
 
-export default Player;
+
+  const opts: YouTubeProps['opts'] = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  return <YouTube videoId={videoCode.videoCode} opts={opts} onReady={onPlayerReady} onPlay={onPlayerPlay} onPause={onPlayerPause} />;
+}
