8384f6eb4d31689bbb9bb0a2f766049f7b60e63a
Hamalain3n <thomas.hamalainen@live.se>
2023-12-15 15:33:10 +0100

    new api key

--- src/app/[room]/page.tsx
+++ src/app/[room]/page.tsx
@@ -8,15 +8,7 @@ export default function room({ params }: { params: { room: string } }) {
   socket.emit('joinRoom', params.room)
   return (
     <main>
-      <h1>room: {params.room}</h1>
       <SearchPage />
-      <button
-        onClick={() => {
-          videoCode = "EpxrDorr1i8";
-        }}
-      >
-        annnanna videeeo
-      </button>
       <Player room={params.room} />
       <div id="player"></div>
     </main>
