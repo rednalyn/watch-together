39a12165b30f0710852b6f13395f07981261cc00
Hamalain3n <thomas.hamalainen@live.se>
2024-02-22 22:57:44 +0100

    add files

---
+++ src/app/room/[room]/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import SearchPage from "../../../components/search";
+import socket from "@/src/api/socketApi";
+import Player from "@/src/components/player";
+
+export default function room({ params }: { params: { room: string } }) {
+  socket.emit("joinRoom", params.room);
+  return (
+    <main>
+      <SearchPage />
+      <Player room={params.room} />
+      <div id="player"></div>
+    </main>
+  );
+}
